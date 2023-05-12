9ed22463c6cd43f282283b3b55af9a6434206718
Luis <luisperi18@hotmail.com>
2023-05-12 17:22:07 +0200

    Add husky commitlint jest sass slint babel and typescript

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+export default {
+  preset: "ts-jest",
+  // TestEnvironment: "jsdom",
+  testPathIgnorePatterns: ["dist"],
+  testpathignorepatterns: ["dist"],
+  resolver: "jest-ts-webcompat-resolver",
+};
